63b40f18fb86dfa6e5224b4a70dd1d0ff22966e3
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-03-27 09:43:14 -0300

    Get Permissao e Permissoes

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
-package medfuturalib
+package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Response struct {
-	Status  bool        `json:"status"`
-	Message string      `json:"message"`
-	Data    Funcionario `json:"data"`
+	Status  bool   `json:"status"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+type JWTResponse struct{
+	Username	string `json:"username"`
+	Password	string `json:"password"`
+	Token 		string `json:"token"`
 }
 
 type Modulo struct {
@@ -39,31 +48,83 @@ func init() {
 	}
 }
 
-func GetPermissoes(coduser string) (*Funcionario, error) {
+func GetPermissoes(coduser int) (*Funcionario, error) {
 
-	req, err := http.NewRequest("GET", os.Getenv("LINK")+coduser, nil)
+	req, err := http.NewRequest("GET", os.Getenv("LINK")+"/auth/"+strconv.Itoa(coduser), nil)
 	if err != nil {
 		return nil, errors.New("erro ao criar requisição: " + err.Error())
 	}
 
-	req.Header.Set("Authorization", os.Getenv("JWT"))
+	jwt, err := GetJWT()
+	if err != nil {
+		return nil, err
+	}
 
-	return RequestResponse(req)
+	req.Header.Set("Authorization", jwt)
+
+	data, err := RequestResponse(req)
+	if err != nil{
+		return nil, err
+	}
+
+	return ToFuncionario(data)
 }
 
-func GetPermissao(coduser string, codmodulo string) (*Funcionario, error) {
+func GetPermissao(coduser int, codmodulo int) (*Funcionario, error) {
 
-	req, err := http.NewRequest("GET", os.Getenv("LINK")+coduser+"?modulo="+codmodulo, nil)
+	req, err := http.NewRequest("GET", os.Getenv("LINK")+"/auth/"+strconv.Itoa(coduser)+"?modulo="+strconv.Itoa(codmodulo), nil)
 	if err != nil {
 		return nil, errors.New("erro ao criar requisição: " + err.Error())
 	}
 
-	req.Header.Set("Authorization", os.Getenv("JWT"))
+	jwt, err := GetJWT()
+	if err != nil {
+		return nil, err
+	}
 
-	return RequestResponse(req)
+	req.Header.Set("Authorization", jwt)
+
+	data, err := RequestResponse(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return ToFuncionario(data)
+
 }
 
-func RequestResponse(req *http.Request) (*Funcionario, error) {
+func GetJWT() (string, error) {
+	body := map[string]string{
+		"username": os.Getenv("USER_SIAC"),
+		"password": os.Getenv("SENHA_SIAC"),
+	}
+
+	jsonData, err := json.Marshal(body)
+	if err != nil {
+		return "", errors.New("Erro ao codificar corpo da requisicao: " + err.Error())
+	}
+
+	req, err := http.NewRequest("POST", os.Getenv("LINK")+"/login", bytes.NewBufferString(string(jsonData)))
+	if err != nil {
+		return "", errors.New("erro ao criar requisição: " + err.Error())
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	data, err := RequestResponse(req)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := ToJWTResponse(data)
+	if err != nil {
+		return "", err
+	}
+
+	return "Bearer " + resp.Token, nil
+}
+
+func RequestResponse(req *http.Request) (any, error) {
 
 	client := &http.Client{}
 	response, err := client.Do(req)
@@ -78,7 +139,7 @@ func RequestResponse(req *http.Request) (*Funcionario, error) {
 	}
 
 	if response.StatusCode != 200 {
-		return nil, errors.New("erro ao acessar a API")
+		return nil, errors.New("erro ao acessar a API: " + string(data))
 	}
 
 	var resp Response
@@ -88,5 +149,45 @@ func RequestResponse(req *http.Request) (*Funcionario, error) {
 		return nil, errors.New("Erro ao decodificar JSON: " + err.Error())
 	}
 
-	return &resp.Data, nil
+	return resp.Data, nil
+}
+
+func ToFuncionario(data any) (*Funcionario, error) {
+
+	dataJson, err := json.Marshal(data)
+	if err != nil {
+		return nil, errors.New("erro ao codificar json: " + err.Error())
+	}
+
+	var funcionario Funcionario
+
+	err = json.Unmarshal(dataJson, &funcionario)
+	if err != nil {
+		return nil, errors.New("erro ao fazer a conversao para funcionario: " + err.Error())
+	}
+
+	return &funcionario, nil
+}
+
+func ToJWTResponse(data any) (*JWTResponse, error) {
+
+	dataJson, err := json.Marshal(data)
+	if err != nil {
+		return nil, errors.New("erro ao codificar json: " + err.Error())
+	}
+
+	var resp JWTResponse
+
+	err = json.Unmarshal(dataJson, &resp)
+	if err != nil {
+		return nil, errors.New("erro ao fazer a conversao: " + err.Error())
+	}
+
+	return &resp, nil
+}
+
+func main() {
+
+	fmt.Println(GetPermissao(327, 46))
+
 }
